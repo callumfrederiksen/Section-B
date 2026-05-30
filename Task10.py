d36650bf04c0bec6983265dc8e4f03fc91c0b9a2
callumfrederiksen <callumfrederiksen97@gmail.com>
2026-05-30 15:27:23 +0100

    Task 10

---
+++ Task10.py
@@ -0,0 +1,17 @@
+print("Enter a whole number: ")
+Number1 = int(input())
+
+print("Enter another whole number: ")
+Number2 = int(input())
+
+Temp1 = Number1
+Temp2 = Number2
+
+while Temp1 != Temp2:
+    if Temp1 > Temp2:
+        Temp1 = Temp1 - Temp2 
+    else:
+        Temp2 = Temp2 - Temp1
+
+Result = Temp1
+print(Result, "is the GCF of", Number1, "and", Number2)
